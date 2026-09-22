38351808b58ff8afda479047eb87162011e986e8
KinzaSoftware04 <kinzajaved022@gmail.com>
2026-09-22 10:28:46 +0500

    Add files via upload

---
+++ netflix_check.py
@@ -0,0 +1,11 @@
+#check Netflix premium status
+def check_netflix_status(has_premium):
+    if has_premium == True:
+        print("welcome! you can watch this  movie in Ultra HD")
+    else:
+        print("please buy a subscription")
+check_netflix_status(True)
+check_netflix_status(False)
+
+    
+         
